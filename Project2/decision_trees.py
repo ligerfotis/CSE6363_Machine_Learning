5ea62e75197f3a49ba203e4b2ec6205b7cc63908
ligerfotis <ligerfotis@gmail.com>
2021-04-14 18:35:35 +0300

    prediction working

--- Project2/decision_trees.py
+++ Project2/decision_trees.py
@@ -12,7 +12,7 @@ def load_data():
     return data_train, data_test
 
 
-x_train, x_test = load_data()
+train_data, test_data = load_data()
 
 
 def entropy(labels, base=None):
@@ -37,14 +37,14 @@ def information_gain(data, split_name):
     if 'x' in values:
         split1 = data[data[split_name] == "x"]
         split1_entropy = entropy(split1['label'], base=BASE)
-        weight = counts[np.where(values == 'x')]/sum(counts)
+        weight = counts[np.where(values == 'x')] / sum(counts)
         weighted_entropy_split1 = weight * split1_entropy
     else:
         weighted_entropy_split1 = 0
     if 'o' in values:
         split2 = data[data[split_name] == "o"]
         split2_entropy = entropy(split2['label'], base=BASE)
-        weight = counts[np.where(values == 'o')]/sum(counts)
+        weight = counts[np.where(values == 'o')] / sum(counts)
         weighted_entropy_split2 = weight * split2_entropy
     else:
         weighted_entropy_split2 = 0
@@ -67,14 +67,13 @@ def best_split(data):
 
     if len(gain_dict) > 0:
         best_feature = max(gain_dict, key=gain_dict.get)
-        best_gain = gain_dict[best_feature]
-    print("Max Information Gain: {} at feature: {}".format(best_gain, best_feature))
+        best_gain = gain_dict[best_feature][0]
+    # print("Max Information Gain: {} at feature: {}".format(best_gain, best_feature))
     return best_feature, best_gain
 
 
 """
 GAIN RATIO?
-How to do the split?
 """
 
 
@@ -89,7 +88,7 @@ class Node:
         self.name = self.parent_node
         self.entropy = 0
         self.df = data_frame
-        self.type = type    # 'x', 'o' or 'b'
+        self.type = type  # 'x', 'o' or 'b'
 
     def __str__(self):
         return "{}_{}".format(self.split_feature, self.type)
@@ -100,7 +99,7 @@ class Node:
 
 class CART:
     def __init__(self):
-        pass
+        self.root = None
 
     def build_tree(self, data_frame, depth, type, parent):
         if depth < 0:
@@ -113,7 +112,7 @@ class CART:
         if depth <= max_depth:
 
             node.calc_entropy()
-            print("Entropy of node is: {}".format(node.entropy))
+            # print("Entropy of node is: {}".format(node.entropy))
 
             # node is a leaf
             if node.entropy == 0:
@@ -128,8 +127,8 @@ class CART:
                 node.name = type
 
             if split_feature is not None:
-                print("Parent: {} | Node: {} | Depth: {} | Split Feature {}".format(node.parent_node, node, depth,
-                                                                                    split_feature))
+                # print("Parent: {} | Node: {} | Depth: {} | Split Feature {}".format(node.parent_node, node, depth,
+                #                                                                     split_feature))
                 node.split_data_o = data_frame[data_frame[split_feature] == 'o'].drop([split_feature], axis=1)
                 node.split_data_b = data_frame[data_frame[split_feature] == 'b'].drop([split_feature], axis=1)
                 node.split_data_x = data_frame[data_frame[split_feature] == 'x'].drop([split_feature], axis=1)
@@ -139,26 +138,72 @@ class CART:
                 node.child_x = self.build_tree(node.split_data_x, depth + 1, type='x', parent=node)
                 node.child_b = self.build_tree(node.split_data_b, depth + 1, type='b', parent=node)
 
+            return node
+
         # max depth reached; make the node a leaf
         else:
             return node
 
     def predict(self, sample, tree):
-        if tree.name is None:
-            return
+        if tree.entropy == 0:
+            value, counts = np.unique(tree.df['label'], return_counts=True)
+            if len(value) == 2:
+                class_pred = value[0] if counts[0] >= counts[1] else value[1]
+                return class_pred
+            # only one class left in the labels
+            else:
+                return value
 
+        if sample[tree.split_feature] == 'o':
+            class_p = self.predict(sample, tree.child_o)
+        elif sample[tree.split_feature] == 'x':
+            class_p = self.predict(sample, tree.child_x)
+        else:
+            class_p = self.predict(sample, tree.child_b)
+        return class_p
 
-#
+###
+max_depth = 2
+###
 cart = CART()
-tree = cart.build_tree(x_train, 0, type="root", parent=None)
-# predict(x_test, tree)
-#
-# column_names = x_train.columns
-# data_x = x_train[column_names[:-1]]
-# labels = x_train[column_names[-1]]
-#
-# sample = data_x.iloc[0]
-# predict(sample, tree)
+tree = cart.build_tree(train_data, 0, type="root", parent=None)
+labels = test_data['label'].values
+x_train = test_data.loc[:, test_data.columns != 'label']
+count_correct_pred = 0
+for index, sample in x_train.iterrows():
+    prediction = cart.predict(sample, tree)
+    if prediction == labels[index]:
+        count_correct_pred += 1
+accuracy = count_correct_pred / len(labels)
+print("Prediction accuracy for max depth {} is {:.2f}%".format(max_depth, accuracy * 100))
 
-# for index, sample in data_x.iterrows():
-# print(sample.values)
+###
+max_depth = 3
+###
+cart = CART()
+tree = cart.build_tree(train_data, 0, type="root", parent=None)
+
+labels = test_data['label'].values
+x_train = test_data.loc[:, test_data.columns != 'label']
+count_correct_pred = 0
+for index, sample in x_train.iterrows():
+    prediction = cart.predict(sample, tree)
+    if prediction == labels[index]:
+        count_correct_pred += 1
+accuracy = count_correct_pred / len(labels)
+print("Prediction accuracy for max depth {} is {:.2f}%".format(max_depth, accuracy * 100))
+
+###
+max_depth = 4
+###
+cart = CART()
+tree = cart.build_tree(train_data, 0, type="root", parent=None)
+labels = test_data['label'].values
+x_train = test_data.loc[:, test_data.columns != 'label']
+count_correct_pred = 0
+for index, sample in x_train.iterrows():
+    prediction = cart.predict(sample, tree)
+    if prediction == labels[index]:
+        count_correct_pred += 1
+accuracy = count_correct_pred / len(labels)
+print("Prediction accuracy for max depth {} is {:.2f}%".format(max_depth, accuracy * 100))
