d3bfa0ecc23899f33b0e70cb678b0f17ed842224
ligerfotis <ligerfotis@gmail.com>
2021-05-03 00:12:26 +0300

    final changes

--- HW3/clustering.py
+++ HW3/clustering.py
@@ -1,42 +1,20 @@
-from math import sqrt
-
-import numpy as np
+import pandas as pd
 from data import heights, weights, age
+import matplotlib.pyplot as plt
+import scipy.cluster.hierarchy as shc
 
 data_points = []
-clusters = {}
 for i, (h, w, a) in enumerate(zip(heights, weights, age)):
     data_points.append([h, w, a])
-    clusters[i] = [h, w, a]
 
-similarity_matrix = []
+df = pd.DataFrame(data_points, columns=['height', 'weight', 'age'])
 
+plt.figure(figsize=(10, 7))
+plt.title("Dendrograms")
+dend_complete = shc.dendrogram(shc.linkage(df, method='complete'))
+plt.savefig('dendrogram_complete.png')
 
-def dist(a, b):
-    d = [a[0] - b[0], a[1] - b[1], a[2] - b[2]]
-    return sqrt(d[0] * d[0] + d[1] * d[1] + d[2] * d[2])
-
-
-D = {}
-
-for cluster1, cords1 in clusters.items():
-    D[cluster1] = {}
-    for cluster2, cords2 in clusters.items():
-        D[cluster1][cluster2] = dist(cords1, cords2)
-
-# minimum = (-1, float("inf"))
-# tree_list = []
-# element_to_merge = -1
-# while len(D) > 1:
-#     for cluster1, v in D.items():
-#         tmp_min = list(dict(sorted(v.items(), key=lambda item: item[1])).items())[1]
-#         if tmp_min[1] < minimum[1]:
-#             element_to_merge = tmp_min[0]
-#             minimum = ((cluster1, element_to_merge), tmp_min[1])
-#     tree_list.append(minimum)
-#     for point in list(minimum[0]):
-#         D[minimum[0]] = []
-#         del D[point]
-#     # # print("Smallest distance: {}".format(min()))
-#     # for cluster2, d in v.items():
-#     #     print(cluster1, cluster2, d)
+plt.figure(figsize=(10, 7))
+plt.title("Dendrograms")
+dend_single = shc.dendrogram(shc.linkage(df, method='single'))
+plt.savefig('dendrogram_single.png')
--- HW3/self_supervised.py
+++ HW3/self_supervised.py
@@ -1,11 +1,11 @@
-# from data import heights, weights, age, gender
 from data import dataset_labeled, dataset_unlabeled, test_dataset
 import numpy as np
 from sklearn.linear_model import LogisticRegression
 import pandas as pd
-k_list = [1, 2, 3, 4, 5, 6]
-# k = 4
-for k in k_list:
+
+logisticRegr_semi = LogisticRegression()
+
+for k in range(7):
     print("k:{}".format(k))
     x_data_labeled = np.array([sample[0] for sample in dataset_labeled])
     y_data = np.array([0 if sample[1] == 'M' else 1 for sample in dataset_labeled])
@@ -14,46 +14,52 @@ for k in k_list:
     x_data_unlabeled = pd.DataFrame(dataset_unlabeled, columns=['height', 'weight', 'age'])
 
     x_test_labeled = np.array([sample[0] for sample in test_dataset])
+    # convert labels 'M' and 'W' to 0 and 1 respectively
     y_test = np.array([0 if sample[1] == 'M' else 1 for sample in test_dataset])
 
-    # train a learner only on the initially labeled data
-    logisticRegr = LogisticRegression()
-    logisticRegr.fit(x_data_labeled, y_data)
-
-    # semi-supervided training
-    logisticRegr_semi = LogisticRegression()
-
     while len(x_data_unlabeled) > 0:
+        # for any remaining sample less than k
         if len(x_data_unlabeled) < k:
             k = len(x_data_unlabeled)
 
+        # fit the logistic regression
         logisticRegr_semi.fit(x_data_labeled, y_data)
+        if k == 0:
+            break
 
-        # unlabelled data confidence
+        # predictions on the unlabeled data
         predictions = logisticRegr_semi.predict(x_data_unlabeled)
+        # confidence for both classes
         confidence_2class = logisticRegr_semi.predict_proba(x_data_unlabeled)
-
+        # highest confidence from the 2 classes
         confidence = [con[int(pred)]for con, pred in zip(confidence_2class, predictions)]
 
         x_data_unlabeled_sorted = x_data_unlabeled.copy()
         x_data_unlabeled_sorted['predictions'] = predictions
         x_data_unlabeled_sorted['confidence'] = confidence
-
+        # sort unlabeled samples by confidence
         x_data_unlabeled_sorted = x_data_unlabeled_sorted.sort_values(by='confidence', ascending=False)
+        # get sorted indexes
         sorted_idx = list(x_data_unlabeled_sorted.index)
         # add newly labeled data to the labeled dataset
         for i in sorted_idx[:k]:
+            # add the most confident label to the labeled dataset
             x_data_labeled = x_data_labeled.append(x_data_unlabeled.iloc[i], ignore_index=True)
+            # do the same for its label
             y_data = np.concatenate([y_data, [x_data_unlabeled_sorted.iloc[i]['predictions']]])
             # remove recently labeled sample from the unlabeled dataset
+        # remove k top recently labeled sample from the unlabeled dataset
         x_data_unlabeled = x_data_unlabeled.drop(sorted_idx[:k], axis=0)
+        # reset the indices of the unlabeled dataset
         x_data_unlabeled.reset_index(drop=True, inplace=True)
 
-    pred_simple = logisticRegr.predict(x_test_labeled)
+    # pred_simple = logisticRegr.predict(x_test_labeled)
     pred_semi = logisticRegr_semi.predict(x_test_labeled)
 
-    accuracy_simple = (len(y_test) - np.count_nonzero(y_test - pred_simple)) / len(y_test)
+    # accuracy_simple = (len(y_test) - np.count_nonzero(y_test - pred_simple)) / len(y_test)
     accuracy_semi = (len(y_test) - np.count_nonzero(y_test - pred_semi)) / len(y_test)
 
-    print("Simple: {}% accuracy | Semi: {}% accuracy".format(accuracy_simple*100, accuracy_semi*100))
+    # print("Simple: {}% accuracy | Semi: {}% accuracy".format(accuracy_simple*100, accuracy_semi*100))
+    print("Semi: {}% accuracy".format(accuracy_semi*100))
+
 
